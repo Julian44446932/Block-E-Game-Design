35d29ca61854f14a736a834e164e5880072f4c16
Julian44446932 <theroastertoaster67@gmail.com>
2022-01-27 09:59:02 -0600

    HW 1/27

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,32 @@
+#Julian Meyer
+#Make a menu for the game with instructions 
+#We are making a code to play rock paper scissors.
+# #Psuedocode: Have computer randomly generate rock paper or scissors
+# Computer should transfer the user input into numbers
+# Make if statements for each scenario
+# Loop yes or no to reset the game
+# Failsafe for if the input a letter instead of a number
+# Misspelling scenario failsafe
+def menu():
+    print("<**************************>")
+    print("<       WELCOME TO         >")
+    print("<          ROCK            >")
+    print("<          PAPER           >")
+    print("<          SCISORS         >")
+    print("<**************************>")
+
+
+menu()
+
+# user='paper'
+# computer = 1
+# import os
+# os.system('cls')
+# if 'pap' in user:
+#     user = int(1)
+#     print("paper"+str(user))
+# elif 'r' in user:
+#     user=int(2)
+# elif 's' in user:
+#     user=int(3)
+
