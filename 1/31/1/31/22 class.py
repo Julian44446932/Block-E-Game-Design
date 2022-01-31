c21d384ddaeeaac68d0c56b617d895b1e6e99873
Julian44446932 <theroastertoaster67@gmail.com>
2022-01-31 10:00:10 -0600

    1/31

---
+++ 1/31/1/31/22 class.py
@@ -0,0 +1,17 @@
+#JUlian Myer
+#01/31/22
+#Strings array of characters
+#Has many functions
+import os
+os.system('cls')
+myName= "Julians Meyer"
+myStatement=""" My name is so nice because 
+sd
+s
+d
+fd"""
+print("My last name begins with " +myName[6])
+print(myStatement)
+if 'blah' in myStatement:
+    print('true')
+print('expert' not in myStatement)
