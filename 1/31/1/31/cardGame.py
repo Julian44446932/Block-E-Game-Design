ac7ef973dcaf016ac5e162daf7caa9dfad6f0aa1
Julian44446932 <theroastertoaster67@gmail.com>
2022-02-16 09:58:39 -0600

    2/16

---
+++ 1/31/1/31/cardGame.py
@@ -0,0 +1,13 @@
+# CArd War game
+import os, random
+os.system('cls')
+numberCards=[]
+for i in range(2,11):
+    numberCards.append(i)
+    numberCards[i-2]=str(numberCards[i-2])
+print(numberCards)
+suit=['s', 'c', 'h', 'd']
+royals=['J', 'Q', 'K', 'A']
+
+# make a list like this (2,3,4,5,6,7,8,9,10,J,Q,K)
+#make a deck of cards addding each suit
