d51c006b924cdfb846f8b9412bf41f29facf4a11
Julian44446932 <theroastertoaster67@gmail.com>
2022-02-18 09:46:14 -0600

    2/18 class

--- 1/31/1/31/cardGame.py
+++ 1/31/1/31/cardGame.py
@@ -8,6 +8,7 @@ for i in range(2,11):
 print(numberCards)
 suit=['s', 'c', 'h', 'd']
 royals=['J', 'Q', 'K', 'A']
+cardTypes=['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 
 # make a list like this (2,3,4,5,6,7,8,9,10,J,Q,K)
 #make a deck of cards addding each suit
